209e74c7f830120946005b92e79eb85309190112
Mystical-mistletoe <151160385+Mystical-mistletoe@users.noreply.github.com>
2025-03-20 12:25:31 +0500

    Update Program.cs

--- Task_02_01/Program.cs
+++ Task_02_01/Program.cs
@@ -63,3 +63,91 @@ public class FactorialCalculator
 
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+// Файл: Card.cs
+using System;
+
+public class Card
+{
+    public string Number { get; set; }
+    public string ClientFIO { get; set; }
+    public string Validity { get; set; }
+    public string SecureCode { get; set; }
+    public string Type { get; set; }
+
+    // Конструктор по умолчанию
+    public Card()
+    {
+        Type = "Дебетовая"; // Значение по умолчанию
+    }
+
+    // Конструктор с инициализацией номера карты и ФИО клиента
+    public Card(string number, string clientFIO) : this() // Вызов конструктора по умолчанию
+    {
+        Number = number;
+        ClientFIO = clientFIO;
+    }
+
+    // Конструктор с инициализацией всех полей, кроме типа карты
+    public Card(string number, string clientFIO, string validity, string secureCode) : this(number, clientFIO)
+    {
+        Validity = validity;
+        SecureCode = secureCode;
+    }
+
+
+    // Конструктор с инициализацией всех полей
+    public Card(string number, string clientFIO, string validity, string secureCode, string type) : this(number, clientFIO, validity, secureCode)
+
+    {
+        Type = type;
+    }
+
+    public void PrintInfo()
+    {
+        Console.WriteLine("Информация о карте:");
+        Console.WriteLine($"Номер: {Number}");
+        Console.WriteLine($"ФИО клиента: {ClientFIO}");
+        Console.WriteLine($"Срок действия: {Validity}");
+        Console.WriteLine($"Код безопасности: {SecureCode}");
+        Console.WriteLine($"Тип: {Type}");
+        Console.WriteLine();
+    }
+}
+
+// Файл: Program.cs
+using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Создание объектов класса Card с использованием разных конструкторов
+        Card card1 = new Card();
+        Card card2 = new Card("1234567890123456", "Иванов Иван Иванович");
+        Card card3 = new Card("9876543210987654", "Петров Петр Петрович", "12/25", "123");
+        Card card4 = new Card("5555444433332222", "Сидоров Сидор Сидорович", "11/28", "456", "Кредитная");
+
+        // Вызов метода PrintInfo() для каждого объекта
+        card1.PrintInfo();
+        card2.PrintInfo();
+        card3.PrintInfo();
+        card4.PrintInfo();
+
+    }
+}
+
+
+
+
